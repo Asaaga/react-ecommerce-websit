c2058ca222c507a96f037ffe4073434fb1f05bba
Asaaga Simon <simonasarga@gmail.com>
2022-12-30 17:38:30 +0100

    adding product component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-//this is feature one work
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href="/">React Shopping Cart</a>
-      </header>
-      <main>Product List</main>
-      <footer>all right reseaved</footer>
-    </div>
-  );
+import React from 'react';
+import Products from './components/Products';
+import data from './data.json';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      size: '',
+      sort: '',
+    };
+  }
+  render() {
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">React Shopping Cart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products products={this.state.products} />
+            </div>
+            <div className="sidebar">Cart Items</div>
+          </div>
+        </main>
+        <footer>all right reseaved</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
